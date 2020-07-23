05a36f82179a3321a8ac71abb64a5956e9c8cd38
RikeshThapa <rthapa@u.rochester.edu>
2020-07-23 13:29:50 -0500

    forest admin full feature first deployment to production, no permission restrictions

---
+++ routes/bids.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { bids } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('bids');
+
+// This file contains the logic of every route in Forest Admin for the collection bids:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Bid
+router.post('/bids', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Bid
+router.put('/bids/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Bid
+router.delete('/bids/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Bids
+router.get('/bids', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Bids
+router.get('/bids/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Bid
+router.get('/bids/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Bids
+router.get('/bids.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Bids
+router.delete('/bids', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
---
+++ routes/rp-ps.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { rpPs } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('rpPs');
+
+// This file contains the logic of every route in Forest Admin for the collection rpPs:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a RP P
+router.post('/rpPs', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a RP P
+router.put('/rpPs/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a RP P
+router.delete('/rpPs/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of RP Ps
+router.get('/rpPs', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of RP Ps
+router.get('/rpPs/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a RP P
+router.get('/rpPs/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of RP Ps
+router.get('/rpPs.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of RP Ps
+router.delete('/rpPs', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
